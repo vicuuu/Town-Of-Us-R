d5515bacfdc00dc0cc05d5206177de01ecc87390
vicuuu <41231635+vicuuu@users.noreply.github.com>
2025-01-22 06:30:37 +0100

    new customhats

---
+++ source/Patches/CustomHats/SkinsConfigFile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TownOfUs.CustomHats;
+
+public class SkinsConfigFile
+{
+    [JsonPropertyName("hats")] public List<CustomHat> Hats { get; set; }
+}
