b14fb0e5ca419cf655e30d954b2f9067577d7a59
vicuuu <41231635+vicuuu@users.noreply.github.com>
2025-01-22 06:31:57 +0100

    new customhats

--- source/Patches/CustomHats/Patches/CosmeticsCachePatches.cs
+++ source/Patches/CustomHats/Patches/CosmeticsCachePatches.cs
@@ -1,1 +1,15 @@
+using HarmonyLib;
 
+namespace TownOfUs.CustomHats.Patches;
+
+[HarmonyPatch(typeof(CosmeticsCache))]
+internal static class CosmeticsCachePatches
+{
+    [HarmonyPatch(nameof(CosmeticsCache.GetHat))]
+    [HarmonyPrefix]
+    private static bool GetHatPrefix(string id, ref HatViewData __result)
+    {
+        TownOfUs.Logger.LogMessage($"trying to load hat {id} from cosmetics cache");
+        return !CustomHatManager.ViewDataCache.TryGetValue(id, out __result);
+    }
+}
