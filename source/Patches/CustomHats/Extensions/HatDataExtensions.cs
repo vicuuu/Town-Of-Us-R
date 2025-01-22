3ee50b1ef4eb7bdf532b199d9c3fb1d6aa1117c1
vicuuu <41231635+vicuuu@users.noreply.github.com>
2025-01-22 06:32:44 +0100

    new customhats

--- source/Patches/CustomHats/Extensions/HatDataExtensions.cs
+++ source/Patches/CustomHats/Extensions/HatDataExtensions.cs
@@ -1,1 +1,14 @@
+namespace TownOfUs.CustomHats.Extensions;
 
+internal static class HatDataExtensions
+{
+    public static HatExtension GetHatExtension(this HatData hat)
+    {
+        if (CustomHatManager.TestExtension != null && CustomHatManager.TestExtension.Condition.Equals(hat.name))
+        {
+            return CustomHatManager.TestExtension;
+        }
+
+        return CustomHatManager.ExtensionCache.TryGetValue(hat.name, out var extension) ? extension : null;
+    }
+}
